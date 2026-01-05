90e47d811692c9178072db522a21e8254c33ad48
Vízhányó Bence <vizhanyo.bence@diak.szbi-pg.hu>
2026-01-05 12:19:50 +0100

    Update test_simple_calculator_with_mock.py

--- test_simple_calculator_with_mock.py
+++ test_simple_calculator_with_mock.py
@@ -1,8 +1,17 @@
+import pytest
+from simple_calculator import SimpleCaculator
+from unittest.mock import Mock
+
 def test_add_with_mock_history_service():
-    pass
+mock_history = Mock()
+calc = SimpleCaculator(history_service=mock_history)
+result = calc.add()
+assert result == 8
+mock_history.save_operation.assert_called_once()
+mock_history.save_operation.assert_called_with("add", 5, 3, 8)
 
 def test_multiply_with_multiple_mocks():
-    pass
+    
 
 def test_get_history_count_with_mock():
     pass
